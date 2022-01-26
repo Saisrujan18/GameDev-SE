637d8d483aa38cbd3a47e19aaca7a27248fb9246
Manjunath <nath.vasam@gmail.com>
2022-01-26 15:34:14 +0530

    player

---
+++ My project/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInput();
+    }
+
+    void FixedUpdate(){
+        rb.velocity = movement * moveSpeed; 
+    }
+
+    void MovementInput(){
+        float mx = Input.GetAxisRaw("Horizontal");
+        float my = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(mx, my).normalized;
+    }
+}
